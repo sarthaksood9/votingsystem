f8a4cbe22894a7ed1379113a3625304522576eff
sarthaksood9 <sarthaksood9@gmail.com>
2024-04-30 12:00:33 +0530

    navupdates

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { RxAvatar } from "react-icons/rx";
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const navigate=useNavigate();
     return (
         <div className='w-full flex justify-between py-5 px-5 shadow-lg'>
             <div>
-                <h1>VOTEBOX</h1>
+                <h1 className="cursor-pointer" onClick={()=>{navigate('/')}}>VOTEBOX</h1>
             </div>
             <div className='flex gap-6 items-center'>
                 <div className='flex gap-5'>
-                    <div>Votes</div>
-                    <div>blogs</div>
+                    <div className="cursor-pointer"  onClick={()=>{navigate('/')}}>Votes</div>
+                    <div className="cursor-pointer" >blogs</div>
                 </div>
-                <RxAvatar className='text-[2rem]' />
+                <RxAvatar onClick={()=>{navigate('/login')}} className='text-[2rem]' />
             </div>
         </div>
     )
